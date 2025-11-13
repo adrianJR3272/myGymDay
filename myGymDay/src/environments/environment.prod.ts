db774c152c9daf08ab0b7f39abd2ddc4ffc5348f
DESKTOP-2OI4MOA\adrij <adrian.joya.rios.vega@gmail.com>
2025-11-13 14:10:35 +0100

    feat. first  version apk

--- myGymDay/src/environments/environment.prod.ts
+++ myGymDay/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyBpruOdffum2L3f10bTeRFhCAQaRitjfdI',
+    authDomain: 'mygymday.firebaseapp.com',
+    projectId: 'mygymday',
+    storageBucket: 'mygymday.firebasestorage.app',
+    messagingSenderId: '492112146057',
+    appId: '1:492112146057:web:af04ad7c0a5f5ad88f6058',
+    measurementId: 'G-NFWWHYVH22',
+  },
 };
